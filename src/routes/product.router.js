a0ab934181702bcb6820638ff00cff05322ff882
Daniela Correa <correadaniela783@gmail.com>
2023-08-16 21:00:10 -0500

    advance-model-product

---
+++ src/routes/product.router.js
@@ -0,0 +1,21 @@
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+} = require("../controllers/product.controller");
+const express = require("express");
+const { verifyJwt } = require("../utils/verifyJWT");
+
+const routerProduct = express.Router();
+
+routerProduct.route("/").get(getAll).post(verifyJwt, create); //🔐
+
+routerProduct
+  .route("/:id")
+  .get(getOne)
+  .delete(verifyJwt, remove)
+  .put(verifyJwt, update); //🔐
+
+module.exports = routerProduct;
